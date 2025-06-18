5e54fa214009c782ef88b5c833ae47826ab21ce4
sharansyrax <dontgiveupcode@gmail.com>
2025-06-18 23:28:30 +0530

    updated next page to record answers

--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -1,7 +1,25 @@
-import React from "react"
-
-const StartInterview = () => {
-  return <div>start</div>
+"use client"
+import React, { useEffect, useState } from "react"
+import { db } from "@/utils/db"
+import { MockInterview } from "@/utils/schema"
+import { eq } from "drizzle-orm"
+const StartInterview = ({ params }) => {
+  const [interviewData, setInterviewData] = useState()
+  const [MockInterviewQuestion, setMockInterviewQuestion] = useState()
+  const GetInterviewDetails = async () => {
+    const result = await db
+      .select()
+      .from(MockInterview)
+      .where(eq(MockInterview.mockId, params.interviewId))
+    const jsonMockResp = JSON.parse(result[0].jsonMockResp)
+    setMockInterviewQuestion(jsonMockResp)
+    console.log(jsonMockResp)
+    setInterviewData(result[0])
+  }
+  useEffect(() => {
+    GetInterviewDetails()
+  }, [])
+  return <div className="grid grid-cols-1 md:grid-cols-2"></div>
 }
 
 export default StartInterview
