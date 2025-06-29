a359d90d1c3383de774a80e348a915053b9c1436
sharan chinthapally <dontgiveupcode@gmail.com>
2025-06-29 20:31:23 +0530

    Added new favicom

--- app/page.js
+++ app/page.js
@@ -1,22 +1,13 @@
-"use client"
-import { SignUp } from "@clerk/nextjs"
-import { useUser } from "@clerk/nextjs"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
+// app/page.tsx
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
 
 export default function Home() {
-  const { isSignedIn } = useUser()
-  const router = useRouter()
+  const { userId } = auth()
 
-  useEffect(() => {
-    if (isSignedIn) {
-      router.push("/dashboard")
-    }
-  }, [isSignedIn, router])
+  if (userId) {
+    redirect("/dashboard") // Authenticated user
+  }
 
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <SignUp />
-    </div>
-  )
+  redirect("/sign-in") // Not signed in
 }
